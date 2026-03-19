92bb6fcbb2730691c825b9219177211087679d09
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2026-03-19 18:42:14 -0500

    Reorganize commands

--- src/Dobby.hs
+++ src/Dobby.hs
@@ -2,11 +2,15 @@ module Dobby where
 
 -- base
 import Control.Applicative ((<**>))
+import Data.Char (toLower)
 import Data.Foldable (fold)
 import Data.Version (showVersion)
 
 -- dobby
 import Paths_dobby (version)
+
+-- http-client
+import Network.HTTP.Client (Manager)
 
 -- http-client-tls
 import Network.HTTP.Client.TLS (newTlsManager)
@@ -18,42 +22,36 @@ import qualified Options.Applicative as Options
 main :: IO ()
 main = do
   command <- execParser commandParserInfo
-  _manager <- newTlsManager
-  case command of
-    Hello ->
-      putStrLn "Hello"
-
-data Command
-  = Hello
+  manager <- newTlsManager
+  run command manager
 
 commandParserInfo :: Options.ParserInfo Command
 commandParserInfo =
   let
-    commandParser =
-      commandParser2
+    mkCommandMod command =
+      Options.command
+        (fmap toLower (show command))
+        (Options.info
+          (pure command)
+          (fold [Options.progDesc (show command)]))
+  in
+    Options.info
+      (Options.hsubparser (foldMap mkCommandMod [minBound..maxBound])
         <**> Options.helper
-        <**> Options.simpleVersioner (showVersion version)
-    mods =
-      [ Options.fullDesc
-      , Options.header mempty
-      , Options.progDesc mempty
-      , Options.footer mempty
-      ]
-  in
-    Options.info commandParser (fold mods)
+        <**> Options.simpleVersioner (showVersion version))
+      (fold
+        [ Options.fullDesc
+        , Options.header mempty
+        , Options.progDesc mempty
+        , Options.footer mempty
+        ])
 
-commandParser2 :: Options.Parser Command
-commandParser2 =
-  let
-    helloParserInfo =
-      let
-        mods =
-          [ Options.progDesc "Hello"
-          ]
-      in
-        Options.info (pure Hello) (fold mods)
-    cmds =
-      [ Options.command "hello" helloParserInfo
-      ]
-  in
-    Options.hsubparser (fold cmds)
+data Command
+  = Hello
+  deriving (Bounded, Enum, Eq, Show)
+
+run :: Command -> Manager -> IO ()
+run command _ =
+  case command of
+    Hello ->
+      putStrLn "Hello"
